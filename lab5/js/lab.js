9e70beb2544f70c79ed222163abb2ea50d72d32c
Alex Wooddy <dragrooncoms@gmail.com>
2020-09-15 18:23:34 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,22 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: 15th of September
+license: Public Domain
+*/
+
+
+// Define Variables
+make = "Ford";
+model = "Taurus";
+color = "Rusty";
+year = 1995;
+
+//calculate
+age = 2019 - year;
+
+// output;
+document.writeln("Make: " + make + "<br>")
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
