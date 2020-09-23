bcd61e505524c5df7f9c830b29ed2b7be69d4b59
Alex Wooddy <dragrooncoms@gmail.com>
2020-09-23 01:14:31 -0700

    lab 7

---
+++ lab7/js/lab7.js
@@ -0,0 +1,28 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: 15th of September
+license: Public Domain
+*/
+
+
+// Define Variables
+function sortUserName () {
+	var userName = window.prompt("Hi. Welcome to the forest, what is your name?");
+	console.log("userName =", userName);
+	//split string to array 
+	var nameArray = userName.split('');
+	console.log("nameArray =", nameArray);
+	//sort the array
+	var nameArraySort = nameArray.sort();
+	console.log("nameArraySort =", nameArraySort);
+	// join array back to string
+	var nameSorted = nameArraySort.join('');
+	console.log("nameSorted =" + nameSorted);
+
+	return nameSorted;
+}
+	//output
+	document.writeln("This is the dumb name the prince of fools has gifted you: ", 
+		sortUserName(), "</br>");
+	
+
