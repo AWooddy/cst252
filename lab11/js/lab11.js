6b0b1bfbffc16bcfe7d0b4dd109699f56484b8af
Alex Wooddy <dragrooncoms@gmail.com>
2020-10-07 14:21:19 -0700

    fx

--- lab11/js/lab11.js
+++ lab11/js/lab11.js
@@ -3,7 +3,7 @@ Author: Alex Wooddy <awooddy@csumb.edu>
 created: 15th of September
 license: Public Domain
 
-code referenced from Wes Modes (Csumb) lecture.
+code referenced from Wes Modes (Csumb) lecture/Kariya Hunter
 */
 
 function sortingHat(str) {
@@ -13,31 +13,31 @@ function sortingHat(str) {
 
 	if (mod == 0) {
 
-		return "Witch"
+		return "Witch";
 	}
 
 	else if (mod == 1) {
-		return"Druid"
+		return"Druid";
 	}
 
 	else if (mod == 2) {
-		return "Necromancer"
+		return "Necromancer";
 	}
 
 	else if (mod == 3) {
-		return "Cleric"
+		return "Cleric";
 	}
 }
 
 
-var button = document.getElementById("button")
-var input = document.getElementById("input")
+var button = document.getElementById("button");
+var input = document.getElementById("input");
 
 
 button.addEventListener("click", function() {
 	var name = document.getElementById("input").value;
-	var house = sortingHat (name);
-	newText = "<p> Congratulations, you are" + house + "class</p>";
-	document.getElementById ("output").innerhtml = newText;
+	var house = sortingHat(name);
+	newText = "<p> Congratulations, you are " + house + " class</p>";
+	document.getElementById ("output").innerHTML = newText;
 
 })
