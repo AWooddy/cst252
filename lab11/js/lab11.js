33b5c65ee65ed70f51b869e4fe6cbcf40f5bf45b
Alex Wooddy <dragrooncoms@gmail.com>
2020-10-06 20:07:15 -0700

    lab 11

---
+++ lab11/js/lab11.js
@@ -0,0 +1,43 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: 15th of September
+license: Public Domain
+
+code referenced from Wes Modes (Csumb) lecture.
+*/
+
+function sortingHat(str) {
+
+	len = str.length;
+	mod = len % 4
+
+	if (mod == 0) {
+
+		return "Witch"
+	}
+
+	else if (mod == 1) {
+		return"Druid"
+	}
+
+	else if (mod == 2) {
+		return "Necromancer"
+	}
+
+	else if (mod == 3) {
+		return "Cleric"
+	}
+}
+
+
+var button = document.getElementById("button")
+var input = document.getElementById("input")
+
+
+button.addEventListener("click", function() {
+	var name = document.getElementById("input").value;
+	var house = sortingHat (name);
+	newText = "<p> Congratulations, you are" + house + "class</p>";
+	document.getElementById ("output").innerhtml = newText;
+
+})
