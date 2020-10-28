7022017a6a6b67a76738303453570a51db0f11c5
Alex Wooddy <dragrooncoms@gmail.com>
2020-10-27 21:02:00 -0700

    lab 15

---
+++ lab 15/js/lab.js
@@ -0,0 +1,43 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: october 20, 2020
+license: Public Domain
+
+code referenced from Wes Modes (Csumb) lecture.
+*/
+
+class Car {
+	constructor (make, model,year,color,extras) {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.color = color;
+	this. extras = extras;
+}
+
+info() {
+	return(  "make:" + this.make + "<br>" +
+		     "model:" + this.model +"<br>" +
+		     "year:" + this.year + "<br>" +
+		     "color:" + this.color + "<br>" +
+		     "extras:" + this.extras);
+  }
+}
+
+var acura = new Car("Acura", "Legend", "1996", "Black", "four door");
+acura.info();
+
+var honda = new Car ("Honda", "Civic", "2006", "Grey", "is not that old");
+honda.info();
+
+document.getElementById("output").innerHTML += "<p>" + acura.info() + "</p>";
+
+document.getElementById("output").innerHTML += "<p>" + honda.info() + "</p>";
+
+
+
+
+
+
+
+
