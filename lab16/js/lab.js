e2c0d8ed03e4d19646f65b3b7b6d38a8f39d0ceb
Alex Wooddy <dragrooncoms@gmail.com>
2020-11-01 21:41:14 -0800

    17/16

---
+++ lab16/js/lab.js
@@ -0,0 +1,23 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: october 27, 2020
+license: Public Domain
+
+code referenced from Wes Modes (Csumb) lecture.
+*/
+
+$("#content").append("<button id='button'> Click me!")
+
+   $("#button").click(function(){
+   	 	alert("The possum hunts once more, tie the consquences and unite the cords");
+   })
+
+   	$("#button").click(function(){
+    $("button").addClass("Blood");
+   	})
+
+
+
+
+
+
---
+++ lab17/js/lab.js
@@ -0,0 +1,18 @@
+/*
+Author: Alex Wooddy <awooddy@csumb.edu>
+created: november 1, 2020
+license: Public Domain
+
+code referenced from Wes Modes (Csumb) lecture.
+*/
+	$("#button1").click(function(){
+       $("#button1").toggleClass("axe");
+   })
+
+	$("#button2").click(function(){
+       $("#button2").toggleClass("cane");
+   })
+
+	$("#button3").click(function(){
+       $("#button3").toggleClass("sword");
+   })
